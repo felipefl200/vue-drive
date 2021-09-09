8b75dfa43ac538c8973dabd6d2278c908008ccb0
felipefl200 <felipefl200@gmail.com>
2021-09-09 16:19:02 -0300

    Option API Vue 3

--- src/pages/MyFiles.vue
+++ src/pages/MyFiles.vue
@@ -7,53 +7,35 @@
       <button class="rounded-button">
         <icon-arrow-up />
       </button>
-    </div>
-    <div class="row">
-      <div class="col-md-3" v-for="item in 6">
-        <div class="card mb-4">
-          <div class="card-body text-center py-5">
-            <icon-type-common height="4em" width="4em" />
-          </div>
-          <div class="card-footer">
-            <div class="d-flex align-items-center">
-              <icon-type-common />
-              <span class="file-name">File {{ item }}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card mb-4 selected-file">
-          <div class="card-body text-center py-5">
-            <icon-type-pdf height="4em" width="4em" />
-          </div>
-          <div class="card-footer">
-            <div class="d-flex align-items-center">
-              <icon-type-pdf />
-              <span class="file-name">File 7.pdf</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card mb-4">
-          <img class="file-thumb" src="https://picsum.photos/id/1015/400/160" />
-          <div class="card-footer">
-            <div class="d-flex align-items-center">
-              <icon-type-image />
-              <span class="file-name">File 8.png</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </div>   
+    <FilesList :files="files" />
   </div>
 </template>
 
 <script>
+import filesApi from "../api/files";
 import ActionBar from "../components/ActionBar.vue";
+import IconTypeCommon from "../components/icons/IconTypeCommon.vue";
+import FilesList from '../components/files/FilesList.vue'
 
 export default {
-  components: { ActionBar },
+  components: { ActionBar, IconTypeCommon, FilesList },
+
+  mounted() {
+    this.fetchFiles();
+  },
+  data: () => ({
+    files: [],
+  }),
+  methods: {
+    async fetchFiles() {
+      try {
+        const { data } = await filesApi.index();
+        this.files = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
